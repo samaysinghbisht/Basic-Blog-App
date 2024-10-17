604bf8ff8a2d6019716e15089c2dee8786d6610d
Samay Singh Bisht <samay.samay987@gmail.com>
2024-10-17 04:25:16 +0200

    updating posts and comments services to allow CORS

--- comments/index.js
+++ comments/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
@@ -11,7 +13,7 @@ app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req,res) => {
+app.post('/posts/:id/comments', ( req, res) => {
     const commentId = randomBytes(4).toString('hex');
 
     const { content } = req.body;
@@ -21,7 +23,7 @@ app.post('/posts/:id/comments', (req,res) => {
     comments.push({ id: commentId, content});
     
     commentsByPostId[req.params.id] = comments;
-    
+
     res.status(201).send(comments);
 });
 
