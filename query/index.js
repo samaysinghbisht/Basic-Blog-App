5145809c5b3507b5b1b8d20bf00098462efddb91
Samay Singh Bisht <samay.samay987@gmail.com>
2024-10-18 17:11:01 +0200

    updating event bus and query service to handle missing events in case query service goes down

--- query/index.js
+++ query/index.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-    const { type, data } = req.body;
+const handleEvent = (type, data) => {
     if (type === 'PostCreated' ) {
         const { id, title } = data;
 
@@ -35,17 +32,36 @@ app.post('/events', (req, res) => {
         const comment = post.comments.find(comment => {
             return comment.id === id;
         });
+
         comment.status = status;
         comment.content = content;   
     }
+};
 
-    console.log(posts);
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+
+    handleEvent(type, data);
 
     res.send({});
 
-
 });
 
-app.listen(4002, () => {
-    console.log('Listening on port 4002')
+app.listen(4002, async () => {
+    console.log('Listening on port 4002');
+    try {
+        const res = await axios.get("http://localhost:4005/events");
+     
+        for (let event of res.data) {
+          console.log("Processing event:", event.type);
+     
+          handleEvent(event.type, event.data);
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
 })
